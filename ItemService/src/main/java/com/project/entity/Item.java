bfc0dce4a2929a8f5bb71ce40f1ca9e2e79e62f4
Vivian <muranzzw@gmail.com>
2025-08-06 12:37:41 -0700

    update hot reload

--- ItemService/src/main/java/com/project/entity/Item.java
+++ ItemService/src/main/java/com/project/entity/Item.java
@@ -1,14 +1,23 @@
 package com.project.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "items")
-
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Item {
     @Id
     private Long id;
     private String name;
     private String pictureUrl;
     private Double price;
+    private int inverntory;
+
 }
