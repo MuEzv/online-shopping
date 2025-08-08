8a3f617dc9cd8d69e793963f5f8370a57dbd3b40
Vivian <muranzzw@gmail.com>
2025-08-07 21:45:40 -0700

    PaymentService DB config done

--- PaymentService/src/main/java/com/project/service/PaymentServiceImpl.java
+++ PaymentService/src/main/java/com/project/service/PaymentServiceImpl.java
@@ -1,15 +1,17 @@
 package com.project.service;
 
 import com.datastax.astra.client.Database;
+import com.project.config.AstraDBConnection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class OrderServiceImpl implements  OrderService{
-    private final Database database;
+public class PaymentServiceImpl implements PaymentService {
+
+    private  final Database database;
 
     @Autowired
-    public OrderServiceImpl(com.project.config.AstraDBConnection connection) {
+    public PaymentServiceImpl(AstraDBConnection connection){
         this.database = connection.getDatabase();
     }
 
--- PaymentService/src/test/java/com/project/service/PaymentServiceApplicationTests.java
+++ PaymentService/src/test/java/com/project/service/PaymentServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.project.paymentservice;
+package com.project.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
