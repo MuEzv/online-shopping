382d5cf5909c895604b0fb5baaf0648e9c4b6665
jye0829 <yechun666666@gmail.com>
2025-08-11 13:21:50 -0700

    feat(accountservice): add JWT auth server user-specific access restriction yet

--- AccountService/src/main/java/com/project/accountservice/controller/AccountController.java
+++ AccountService/src/main/java/com/project/accountservice/controller/AccountController.java
@@ -10,6 +10,8 @@ import com.project.accountservice.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,6 +31,7 @@ public class AccountController {
     }
 
     @PutMapping("/{email}")
+    @PreAuthorize("#email == authentication.name")
     public ResponseEntity<AccountResponseDTO> updateAccount(
             @PathVariable String email,
             @RequestBody AccountRequestDTO request) {
@@ -38,6 +41,7 @@ public class AccountController {
     }
 
     @GetMapping("/{email}")
+    @PreAuthorize("#email == authentication.name")
     public ResponseEntity<AccountResponseDTO> getAccount(@PathVariable String email) {
         return service.getAccountByEmail(email)
                 .map(ResponseEntity::ok)
