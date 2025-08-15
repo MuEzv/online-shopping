7ab7e75ee690abab8a1c0eed2f11b71ee12097de
jye0829 <yechun666666@gmail.com>
2025-08-15 14:32:03 -0700

    swagger

---
+++ AccountService/src/main/java/com/project/accountservice/config/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package com.project.accountservice.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info; // ✅ 正确的import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Account Service API")
+                        .version("1.0.0")
+                        .description("API documentation for the Account Service of the e-commerce application"));
+    }
+}
